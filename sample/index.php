e6544649ca48665199c0bc66cca6ec795f79f4c1
Javier Zapata <javierzapata82@gmail.com>
2014-10-25 17:46:38 +0200

    Update sample

--- sample/index.php
+++ sample/index.php
@@ -9,21 +9,21 @@ use nkm\RedsysVirtualPos\Message\WebRequest;
 use nkm\RedsysVirtualPos\Message\WebResponse;
 use nkm\RedsysVirtualPos\Field\Currency;
 
-$env = new nkm\RedsysVirtualPos\Environment\LiveEnvironment();
-$env->setSecret('ab12cd3e4fg56h7ijk89');
+$env = new nkm\RedsysVirtualPos\Environment\TestEnvironment();
+$env->setSecret('qwertyasdf0123456789');
 
 $webRequest = new WebRequest($env);
 $webRequest->setParams([
     'amount'             => '24995',
     'currency'           => Currency::EUR,
-    'merchantCode'       => '345678901',
-    'merchantName'       => 'Comercio en pruebas',
+    'merchantcode'       => '327234688',
+    'merchantname'       => 'Comercio en pruebas',
     'order'              => '141020090000',
-    'productDescription' => 'Descripción del producto',
+    'productdescription' => 'Descripción del producto',
     'terminal'           => '1',
-    'merchantUrl'        => 'https://example.com/redsys/receiver',
-    'urlOk'              => 'https://example.com/order/checkout/confirmation.html',
-    'urlKo'              => 'https://example.com/order/checkout/failure.html',
+    'merchanturl'        => 'https://example.com/redsys/receiver',
+    'urlok'              => 'https://example.com/order/checkout/confirmation.html',
+    'urlko'              => 'https://example.com/order/checkout/failure.html',
 ]);
 
 $submitBtn = "<input type='submit' name='submit' value='Submit'>";
@@ -33,20 +33,20 @@ var_dump($webRequest->getValidationErrors());
 
 $webResponse = new WebResponse($env);
 $webResponse->setParams([
-    'Ds_TransactionType'   => '0',
-    'Ds_Card_Country'      => '280',
-    'Ds_Date'              => '20/10/2014',
-    'Ds_SecurePayment'     => '1',
-    'Ds_Signature'         => 'B3BA068B98FFFE4F2D717DA35E8855D10BDE4798',
-    'Ds_Order'             => '141020090000',
-    'Ds_Hour'              => '09:00',
-    'Ds_Response'          => '0000',
-    'Ds_AuthorisationCode' => '123456',
-    'Ds_Currency'          => '978',
-    'Ds_ConsumerLanguage'  => '5',
-    'Ds_MerchantCode'      => '345678901',
-    'Ds_Amount'            => '24995',
-    'Ds_Terminal'          => '001',
+    'ds_transactiontype'   => '0',
+    'ds_card_country'      => '280',
+    'ds_date'              => '20/10/2014',
+    'ds_securepayment'     => '1',
+    'ds_signature'         => '7D9FB1CA60B10FB6A5B7CE883EA3C8A1F8015D3B',
+    'ds_order'             => '141020090000',
+    'ds_hour'              => '09:00',
+    'ds_response'          => '0000',
+    'ds_authorisationcode' => '123456',
+    'ds_currency'          => '978',
+    'ds_consumerlanguage'  => '5',
+    'ds_merchantcode'      => '327234688',
+    'ds_amount'            => '24995',
+    'ds_terminal'          => '001',
 ]);
 
 var_dump($webResponse->getIsValid());
