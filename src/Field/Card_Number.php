6566b06237161ae6bab65935d85c3a70571838a7
Javier Zapata <javierzapata82@gmail.com>
2021-03-25 12:04:05 +0100

    Add undocumented field `DS_Card_Number`

---
+++ src/Field/Card_Number.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Redsys Virtual POS
+ *
+ * @package    Redsys Virtual POS
+ * @author     Javier Zapata <javierzapata82@gmail.com>
+ * @copyright  2021 Javier Zapata <javierzapata82@gmail.com>
+ * @license    https://opensource.org/licenses/MIT The MIT License
+ * @link       https://github.com/jzfgo/redsys-virtual-pos
+ */
+
+namespace nkm\RedsysVirtualPos\Field;
+
+/**
+ * Holds the value of a request/response parameter
+ *
+ * @package    Redsys Virtual POS
+ * @author     Javier Zapata <javierzapata82@gmail.com>
+ * @copyright  2021 Javier Zapata <javierzapata82@gmail.com>
+ * @license    https://opensource.org/licenses/MIT The MIT License
+ * @link       https://github.com/jzfgo/redsys-virtual-pos
+ */
+class Card_Number extends AbstractField implements FieldInterface
+{
+    /**
+     * Indicates if this field can appear in a response
+     *
+     * @var boolean
+     */
+    protected $inResponse = true;
+}
